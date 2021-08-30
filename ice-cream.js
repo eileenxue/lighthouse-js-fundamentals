07c290646f91623e1504399ffac747a74189cf1e
Eileen Xue <xue.eileen@gmail.com>
2021-08-30 20:32:12 +0000

    Array assignments

---
+++ ice-cream.js
@@ -0,0 +1,16 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+// First flavour
+console.log(iceCreamFlavours[0]);
+
+// Last flavour
+console.log(iceCreamFlavours[5]);
+
+// Total flavours
+console.log(iceCreamFlavours.length);
